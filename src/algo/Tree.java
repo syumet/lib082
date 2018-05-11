daf5af18cc00c64113da050b584f3c501b92c320
wxlxy95 <liuxinyu9090@gmail.com>
2018-05-10 22:12:49 -0600

    solution to LC #104

--- src/algo/Tree.java
+++ src/algo/Tree.java
@@ -34,6 +34,15 @@ public class Tree<E> {
     	return (isSame(p.getLeft(), q.getLeft()) && isSame(p.getRight(), q.getRight()));
     }
     
+	/**
+	 * LeetCode Problem #104
+	 * See https://leetcode.com/problems/maximum-depth-of-binary-tree/description/ for details.
+	 */
+    public static <E> int maxDepth(BinaryTree<E> root) {
+    	if (root == null) return 0;
+    	return 1 + Integer.max(maxDepth(root.getLeft()), maxDepth(root.getRight()) );
+	}
+    
     public static void main(String[] args) {
 		
     	// test for invert()
@@ -53,8 +62,10 @@ public class Tree<E> {
 		
     	Character[] w1 = {'A','B','C','D','E','G','H','I','J'};
 		T1 = new BinaryTree<>(w1);
-		System.out.println(isSame(T, T1));
+		System.out.print(isSame(T, T1) + "\t");
 		
+		// test for maxDepth()
+		System.out.println(maxDepth(T));
 	}
     
 }
