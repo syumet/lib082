6a9dbe2a2725ded8c653a240bf6918e12d157b30
wxlxy95 <liuxinyu9090@gmail.com>
2018-05-13 13:21:33 -0600

    Use TreeNode Class defined internally.

--- src/algo/Tree.java
+++ src/algo/Tree.java
@@ -3,7 +3,15 @@ package algo;
 import java.util.LinkedList;
 import java.util.Queue;
 
-import ds.BinaryTree;
+/**
+ * A simple definition for a binary tree node given by LeetCode.
+ */
+ class TreeNode {
+	 int val;
+	 TreeNode left;
+	 TreeNode right;
+	 TreeNode(int x) { val = x; }
+ }
 
 public class Tree<E> {
 
@@ -11,14 +19,14 @@ public class Tree<E> {
 	 * LeetCode Problem #226
 	 * See https://leetcode.com/problems/invert-binary-tree/description/ for details.
 	 */
-    public static <E> BinaryTree<E> invert(BinaryTree<E> root) {
+    public static TreeNode invert(TreeNode root) {
     	
-        if (root == null) return null; 
-        
-		BinaryTree<E> left = root.getLeft();
-		BinaryTree<E> right = root.getRight();
-		root.setLeft(invert(right));
-		root.setRight(invert(left));
+        if (root == null) 
+        	return null; 
+		TreeNode left = root.left;
+		TreeNode right = root.right;
+		root.left = invert(right);
+		root.right = invert(left);
 		
 		return root;
     }
@@ -27,48 +35,48 @@ public class Tree<E> {
 	 * LeetCode Problem #100
 	 * See https://leetcode.com/problems/same-tree/description/ for details.
 	 */
-    public static <E> Boolean isSame(BinaryTree<E> p, BinaryTree<E> q) {
+    public static <E> Boolean isSame(TreeNode p, TreeNode q) {
     	
     	if (p == null && q == null)
     		return true;
-    	if (p == null || q == null || p.getRootItem() != q.getRootItem())
+    	if (p == null || q == null || p.val != q.val)
     		return false;
     	
-    	return (isSame(p.getLeft(), q.getLeft()) && isSame(p.getRight(), q.getRight()));
+    	return (isSame(p.left, q.left) && isSame(p.right, q.right));
     }
     
 	/**
 	 * LeetCode Problem #104
 	 * See https://leetcode.com/problems/maximum-depth-of-binary-tree/description/ for details.
 	 */
-    public static <E> int maxDepth(BinaryTree<E> root) {
+    public static <E> int maxDepth(TreeNode root) {
     	if (root == null) return 0;
-    	return 1 + Integer.max(maxDepth(root.getLeft()), maxDepth(root.getRight()) );
+    	return 1 + Integer.max(maxDepth(root.left), maxDepth(root.right) );
 	}
     
 	/**
 	 * LeetCode Problem #111
 	 * See https://leetcode.com/problems/minimum-depth-of-binary-tree/description/ for details.
 	 */
-    public static <E> int minDepth(BinaryTree<E> root) {
+    public static <E> int minDepth(TreeNode root) {
     	
     	if (root == null) return 0;
     	
-    	Queue<BinaryTree<E>> T = new LinkedList<>();   	
+    	Queue<TreeNode> T = new LinkedList<>();   	
     	Queue<Integer> D = new LinkedList<>();	
     	
     	T.add(root);
     	D.add(1);
-    	BinaryTree<E> cur = T.poll();
+    	TreeNode cur = T.poll();
     	int dep = D.poll();
     	
-    	while (cur.getLeft() != null || cur.getRight() != null) {
-			if (cur.getLeft() != null) {
-				T.add(cur.getLeft());
+    	while (cur.left != null || cur.right != null) {
+			if (cur.left != null) {
+				T.add(cur.left);
 				D.add(dep + 1);
 			}
-			if (cur.getRight() != null) {
-				T.add(cur.getRight());
+			if (cur.right != null) {
+				T.add(cur.right);
 				D.add(dep + 1);
 			}	
 			cur = T.poll();
@@ -77,8 +85,18 @@ public class Tree<E> {
     	return dep;
 	}
     
-    public static void main(String[] args) {
+    public static boolean hasPathSum(TreeNode root, int sum) {
 		
+    	if (root.left == null && root.right == null) {
+    		
+    	}
+    	
+    	return false;
+        
+    }
+      
+    public static void main(String[] args) {
+/*		
     	// test for invert()
     	Character[] words = {'A','B','C','D','E','F','G','H','I','J'};
 		BinaryTree<Character> T = new BinaryTree<>(words);
@@ -101,7 +119,7 @@ public class Tree<E> {
 		// test for maxDepth(), minDepth()
 		System.out.print(maxDepth(T) + "\t");
 		System.out.println(minDepth(T));
-		
+*/		
 	}
     
 }
