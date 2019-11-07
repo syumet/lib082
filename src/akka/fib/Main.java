f5a52f6533adf530c09d220e54086dd64145f614
wxlxy95 <liuxinyu9090@gmail.com>
2019-11-07 13:15:48 -0500

    Use console input instead of argument

--- src/akka/fib/Main.java
+++ src/akka/fib/Main.java
@@ -1,21 +1,22 @@
 package akka.fib;
+
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Read in command line arguments
-        int nthFibonacci = Integer.parseInt(args[0]);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a positive integer:");
+        int nth = scan.nextInt();
+        scan.close();
 
         final ActorSystem system = ActorSystem.create("fib-number");
         final ActorRef master = system.actorOf(
-                MasterActor.props(nthFibonacci), "fib-number-master");
+                MasterActor.props(nth), "fib-number-master");
 
         master.tell(new MasterMessage.Start(), ActorRef.noSender());
     }
 }
-
-
-
