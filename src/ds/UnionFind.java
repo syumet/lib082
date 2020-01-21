363cb72d547fe5ac19fee5bae3b8c7ac775c8ec1
wxlxy95 <liuxinyu9090@gmail.com>
2020-01-21 16:21:50 -0500

    Add ds UnionFind

---
+++ src/ds/UnionFind.java
@@ -0,0 +1,39 @@
+package ds;
+
+import java.util.Arrays;
+
+public class UnionFind<E> {
+
+	int[] parent;
+	int count;
+	
+	public UnionFind(int n) {
+		parent = new int[n];
+		Arrays.fill(parent, -1);
+		count = n;
+	}
+	
+    public void union(int i, int j) {  
+        if (-parent[i] < -parent[j]) {
+        	int tmp = i;
+        	i = j;
+        	j = tmp;
+        }
+        parent[i] += parent[j];
+        parent[j] = i;
+        count--;
+    }
+    
+	public int find (int i) {  
+    	if (parent[i] < 0)
+    		return i;
+    	else {
+    		parent[i] = find(parent[i]);
+    		return parent[i]; 
+    	} 
+    }
+
+    public int getCount() {
+		return count;
+	}
+}
