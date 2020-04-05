b77bb24899fd57ed77f039a7aaf8a59338afb4a7
wxlxy95 <liuxinyu9090@gmail.com>
2020-04-05 01:00:40 -0400

    Adjust code style

--- src/main/java/algo/ksp/RandomKnapsackInstance.java
+++ src/main/java/algo/ksp/RandomKnapsackInstance.java
@@ -7,42 +7,47 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 
 public class RandomKnapsackInstance {
-	public static void main(String args[]) throws IOException {
-		
-		final int MAX_W = 100;    // Largest possible capacity of backpack.
-		final int NUMBER_OF_ITEMS = 700;  // Number of items to be considered for inclusion in the backpack
-		final double MAX_ITEM_VALUE = 1000.0; // Largest possible item value
-		final double MAX_ITEM_WEIGHT = MAX_W; // Largest possible item weight.
-		
-		// Filename is determined by the number of items.
-		final String fileName = "knapsack-" + NUMBER_OF_ITEMS + ".dat";
 
-		// Open file for writing.
-		Writer writer = null;
-		try {
-			writer = new BufferedWriter(new OutputStreamWriter(
-					new FileOutputStream(fileName)));
-			// Write out backpack capacity - random number between 0 and MAX_W
-			writer.write("" + Math.random()* MAX_W  + "\n");
-			// Write out number of items 
-			writer.write("" + NUMBER_OF_ITEMS + "\n");
-		} catch (IOException ex) {
-			System.out.println("Cant open " + fileName);
-			writer.close();
-			return;
-		} 
+  public static void main(String[] args) throws IOException {
 
-		// Generate NUMBER_OF_ITEMS random items and write them to the file.
-		for(int i = 0; i < NUMBER_OF_ITEMS; i++) {
-			// Write the item's value -- random number between 0 and MAX_ITEM_VALUE
-			writer.write(Double.toString(Math.random() * MAX_ITEM_VALUE));
-			writer.write(" ");
-			
-			// Write the items's weight -- random number between 0 and MAX_ITEM_WEIGHT
-			writer.write(Double.toString(Math.random() * MAX_ITEM_WEIGHT));
-			writer.write("\n");
-		}
+    // Largest possible capacity of backpack.
+    final int MAX_W = 100;
+    // Number of items to be considered for inclusion in the backpack
+    final int NUMBER_OF_ITEMS = 700;
+    // Largest possible item value
+    final double MAX_ITEM_VALUE = 1000.0;
+    // Largest possible item weight.
+    final double MAX_ITEM_WEIGHT = MAX_W;
 
-		writer.close();
-	}
+    // Filename is determined by the number of items.
+    final String fileName = "knapsack-" + NUMBER_OF_ITEMS + ".dat";
+
+    // Open file for writing.
+    Writer writer = null;
+    try {
+      writer = new BufferedWriter(new OutputStreamWriter(
+          new FileOutputStream(fileName)));
+      // Write out backpack capacity - random number between 0 and MAX_W
+      writer.write("" + Math.random() * MAX_W + "\n");
+      // Write out number of items
+      writer.write("" + NUMBER_OF_ITEMS + "\n");
+    } catch (IOException ex) {
+      System.out.println("Cant open " + fileName);
+      writer.close();
+      return;
+    }
+
+    // Generate NUMBER_OF_ITEMS random items and write them to the file.
+    for (int i = 0; i < NUMBER_OF_ITEMS; i++) {
+      // Write the item's value -- random number between 0 and MAX_ITEM_VALUE
+      writer.write(Double.toString(Math.random() * MAX_ITEM_VALUE));
+      writer.write(" ");
+
+      // Write the items's weight -- random number between 0 and MAX_ITEM_WEIGHT
+      writer.write(Double.toString(Math.random() * MAX_ITEM_WEIGHT));
+      writer.write("\n");
+    }
+
+    writer.close();
+  }
 }
--- src/main/java/algo/ksp/SackApp.java
+++ src/main/java/algo/ksp/SackApp.java
@@ -5,34 +5,34 @@ import java.util.Random;
 
 public class SackApp {
 
-	public static void main(String[] args) {
-		final int numOfPlunder = 5;
-		Sack[] jacksPlunder = generatePlunder(numOfPlunder);
-		@SuppressWarnings("unchecked")
-		LinkedList<Sack>[] sortedPlunder = new LinkedList[Grain.values().length];
-		for (int i = 0; i < Grain.values().length; i++) {
-			sortedPlunder[i] = new LinkedList<Sack>();
-		}
-		for (int i = 0; i < numOfPlunder; i++) {
-			sortedPlunder[jacksPlunder[i].getType().ordinal()].add(jacksPlunder[i]);
-		}
-		for (int i = 0; i < Grain.values().length; i++) {
-			double totalPounds = 0;
-			for (Sack sk : sortedPlunder[i]) {
-				totalPounds += sk.getWeight();
-			}
-			System.out.println("Jack plundered " + totalPounds + " pounds of " + Grain.values()[i]);
-		}
-	}
+  public static void main(String[] args) {
+    final int numOfPlunder = 5;
+    Sack[] jacksPlunder = generatePlunder(numOfPlunder);
+    @SuppressWarnings("unchecked")
+    LinkedList<Sack>[] sortedPlunder = new LinkedList[Grain.values().length];
+    for (int i = 0; i < Grain.values().length; i++) {
+      sortedPlunder[i] = new LinkedList<>();
+    }
+    for (int i = 0; i < numOfPlunder; i++) {
+      sortedPlunder[jacksPlunder[i].getType().ordinal()].add(jacksPlunder[i]);
+    }
+    for (int i = 0; i < Grain.values().length; i++) {
+      double totalPounds = 0;
+      for (Sack sk : sortedPlunder[i]) {
+        totalPounds += sk.getWeight();
+      }
+      System.out.println("Jack plundered " + totalPounds + " pounds of " + Grain.values()[i]);
+    }
+  }
 
-	public static Sack[] generatePlunder(int howMany) {
-		Random generator = new Random();
-		Sack grain[] = new Sack[howMany];
-		for (int i = 0; i < howMany; i++) {
-			grain[i] = new Sack (
-			Grain.values()[generator.nextInt(Grain.values().length)],
-			generator.nextDouble() * 100 );
-		}
-		return grain ;
-	}
+  public static Sack[] generatePlunder(int howMany) {
+    Random generator = new Random();
+    Sack[] grain = new Sack[howMany];
+    for (int i = 0; i < howMany; i++) {
+      grain[i] = new Sack(
+          Grain.values()[generator.nextInt(Grain.values().length)],
+          generator.nextDouble() * 100);
+    }
+    return grain;
+  }
 }
